d80d6178a123efa28ec2b1bead4855ecfd3059eb
CraigYeoman <yeomancraig@gmail.com>
2022-02-11 08:29:32 -0600

    Operator functions

---
+++ script.js
@@ -0,0 +1,28 @@
+const operate = function(x, a, b) {
+    switch (x) {
+        case "add":
+            return add(a,b);
+        case "substract":
+            return substract(a,b);
+        case "multiply":
+            return multiply(a,b);
+        case "divide":
+            return divide(a,b);  
+    }
+}
+
+const add = function(a, b) {
+    return a + b
+}
+
+const substract = function(a, b) {
+    return a - b
+}
+
+const multiply = function(a, b) {
+    return a * b
+}
+
+const divide = function(a, b) {
+    return a / b
+}
