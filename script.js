a88d5c0fd75152afe6d6825a4b4a89850f79802a
Craig Yeoman <yeomancraig@gmail.com>
2022-02-22 21:10:15 -0600

    reworked if statments and queryselectors

--- script.js
+++ script.js
@@ -39,34 +39,81 @@ const pastInput = document.getElementById('past-input')
 let displayValue = '';
 let pastDisplayValue = '';
 let operator = '';
-let answer = '?';
+let answer = '';
 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        if (button.id === '+' || button.id === '-'
-         || button.id === '*' || button.id === '/') {
+// numberButtons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         if (button.id === '+' || button.id === '-'
+//          || button.id === '*' || button.id === '/') {
             
-            check2();
-        } else if (button.id === '1' || button.id === '2' || button.id === '3' || 
-         button.id === '4' || button.id === '5' || button.id === '6' || 
-         button.id === '7' || button.id === '8' || button.id === '9' || 
-         button.id === '0' || button.id === '.') {
-            check();
-            displayValue += button.id;
-            userInput.textContent = displayValue;
-        } else if (button.id === 'clear') {
-            displayValue = "";
-            pastDisplayValue = "";
-            userInput.textContent = "";
-            pastInput.textContent = "";
-        } else if (button.id === 'delete') {
-            displayValue = "";
-            userInput.textContent = "";
-        } else if (button.id = '=') {
-            equals()
+//             check2();
+//         } else if (button.id === '1' || button.id === '2' || button.id === '3' || 
+//          button.id === '4' || button.id === '5' || button.id === '6' || 
+//          button.id === '7' || button.id === '8' || button.id === '9' || 
+//          button.id === '0' || button.id === '.') {
+//             check();
+//             displayValue += button.id;
+//             userInput.textContent = displayValue;
+//         } else if (button.id === 'clear') {
+//             displayValue = "";
+//             pastDisplayValue = "";
+//             userInput.textContent = "";
+//             pastInput.textContent = "";
+//         } else if (button.id === 'delete') {
+//             displayValue = "";
+//             userInput.textContent = "";
+//         } else if (button.id = '=') {
+//             equals()
+//         }
+//     })
+// })
+
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(answer !== "") {
+            pastInput.textContent = answer
         }
+        displayValue += button.id;
+        userInput.textContent = displayValue;
+        console.log(displayValue);
     })
 })
+
+operationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(pastDisplayValue !== "") {
+            equals();
+            operator = button.id;
+            return
+        }
+        operator = button.id;
+        pastDisplayValue = displayValue;
+        pastInput.textContent = pastDisplayValue + " " + operator;
+        displayValue = "";
+        userInput.textContent = "";
+        console.log(operator);
+    })
+})
+
+clearButton.addEventListener('click', () => {
+    displayValue = "";
+    pastDisplayValue = ""
+    operator = "";
+    answer = "";
+    userInput.textContent = "";
+    pastInput.textContent = "";
+});
+
+deleteButton.addEventListener('click', () => {
+    displayValue = "";
+    userInput.textContent = "";
+});
+
+equalsButton.addEventListener('click', () => {
+    console.log(displayValue)
+    equals();
+});
 
 const check = function() {
     if (displayValue === answer) {
@@ -94,5 +141,6 @@ const equals = function() {
     answer = operate(operator, pastDisplayValue, displayValue);
             userInput.textContent = answer;
             pastInput.textContent = pastDisplayValue + ' ' + operator + ' ' + displayValue;
-            displayValue = answer
+            pastDisplayValue = answer;
+            displayValue = "";
 }
