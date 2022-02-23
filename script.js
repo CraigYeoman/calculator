704783cf035b276b720b089fe8aacfc0b67bae6e
CraigYeoman <yeomancraig@gmail.com>
2022-02-23 06:19:32 -0600

    code clean up/finished javascript

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//Variables
+let displayValue = '';
+let pastDisplayValue = '';
+let operator = '';
+let answer = '';
+
+//QuerySelectors
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+
+//Get ElementbyId
+const userInput = document.getElementById('user-input');
+const pastInput = document.getElementById('past-input');
+
+//Functions
 const operate = function(x, a, b) {
     switch (x) {
         case "+":
@@ -27,52 +45,20 @@ const divide = function(a, b) {
     return Number(a) / Number(b)
 }
 
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equals]');
-const clearButton = document.querySelector('[data-clear]');
-const deleteButton = document.querySelector('[data-delete]');
-
-const userInput = document.getElementById('user-input')
-const pastInput = document.getElementById('past-input')
-
-let displayValue = '';
-let pastDisplayValue = '';
-let operator = '';
-let answer = '';
-
-// numberButtons.forEach((button) => {
-//     button.addEventListener('click', () => {
-//         if (button.id === '+' || button.id === '-'
-//          || button.id === '*' || button.id === '/') {
-            
-//             check2();
-//         } else if (button.id === '1' || button.id === '2' || button.id === '3' || 
-//          button.id === '4' || button.id === '5' || button.id === '6' || 
-//          button.id === '7' || button.id === '8' || button.id === '9' || 
-//          button.id === '0' || button.id === '.') {
-//             check();
-//             displayValue += button.id;
-//             userInput.textContent = displayValue;
-//         } else if (button.id === 'clear') {
-//             displayValue = "";
-//             pastDisplayValue = "";
-//             userInput.textContent = "";
-//             pastInput.textContent = "";
-//         } else if (button.id === 'delete') {
-//             displayValue = "";
-//             userInput.textContent = "";
-//         } else if (button.id = '=') {
-//             equals()
-//         }
-//     })
-// })
+const equals = function() {
+    answer = operate(operator, pastDisplayValue, displayValue);
+            userInput.textContent = answer;
+            pastInput.textContent = pastDisplayValue + ' ' + operator + ' ' + displayValue;
+            pastDisplayValue = answer;
+            displayValue = "";
+};
 
 
+//EventListeners
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
         if(answer !== "") {
-            pastInput.textContent = answer
+            pastInput.textContent = answer + " " + operator;
         }
         displayValue += button.id;
         userInput.textContent = displayValue;
@@ -98,7 +84,7 @@ operationButtons.forEach((button) => {
 
 clearButton.addEventListener('click', () => {
     displayValue = "";
-    pastDisplayValue = ""
+    pastDisplayValue = "";
     operator = "";
     answer = "";
     userInput.textContent = "";
@@ -111,36 +97,6 @@ deleteButton.addEventListener('click', () => {
 });
 
 equalsButton.addEventListener('click', () => {
-    console.log(displayValue)
+    console.log(displayValue);
     equals();
 });
-
-const check = function() {
-    if (displayValue === answer) {
-        pastDisplayValue = displayValue;
-        pastInput.textContent = pastDisplayValue
-        displayValue = ''
-    }
-}
-
-const check2 = function() {
-    if (!(pastDisplayValue === "") && !(operator === "") && !(displayValue === "")) {
-        equals();
-        //(pastDisplayValue === answer) {
-        //pastInput.textContent = pastDisplayValue + ' ' + operator;
-    } else {
-        operator = button.id;
-        pastDisplayValue = displayValue;
-        pastInput.textContent = pastDisplayValue + ' ' + operator;
-        userInput.textContent = "";
-        displayValue = '';
-    }
-}
-
-const equals = function() {
-    answer = operate(operator, pastDisplayValue, displayValue);
-            userInput.textContent = answer;
-            pastInput.textContent = pastDisplayValue + ' ' + operator + ' ' + displayValue;
-            pastDisplayValue = answer;
-            displayValue = "";
-}
